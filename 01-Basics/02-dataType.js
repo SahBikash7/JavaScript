941d9739f5ca4e84adac0ce8bc4a8c145bb9f995
Bikash <sahb2676@gmail.com>
2024-01-03 10:10:33 +0545

    Learned datatypes

---
+++ 01-Basics/02-dataType.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3); //we are using nodejs not browser..
+
+let name = "Bikash"; //String
+let age = 23; //Number has range of 2 to the pow 53
+let isLoggedIn = false; // Boolean
+let state; //Undefined
+
+/* We also have other Datatypes as:
+1.bignit
+2.null (It is a stand alone value)(Representation of an empty value)
+3.undefined (variable defined but not assigned)
+4.symbol (to find uniqueness)
+5.Object*/
+
+console.log(typeof null); //  => Object
+console.log(typeof undefined); //  => undefined
