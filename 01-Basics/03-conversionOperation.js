b2d9d42f1a8c9c9e23a8b0e6ff72ea0449b6c730
Bikash <sahb2676@gmail.com>
2024-01-03 11:10:02 +0545

    Conversion of datatypes

---
+++ 01-Basics/03-conversionOperation.js
@@ -0,0 +1,40 @@
+let score = false;
+// console.log(typeof score);
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// Conversion Of Different Datatypes to Number:
+// 33 => 33;
+// "" => 0
+// "Something" => NaN(NaN(Not a Number) has its datatype as number);
+// null => 0
+// undefined => NaN
+// true => 1; false => 0
+
+let isLoggedIn = undefined;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// Conversion Of Different Datatypes to Boolean:
+// 1 => true;
+// 33 => true;
+// 0 => false;
+// "" => 0
+// "Something" => true;
+// null => false
+// undefined => false
+
+let someNumber = false;
+let stringSomeNumber = String(someNumber);
+// console.log(typeof stringSomeNumber);
+// console.log(stringSomeNumber);
+
+// Conversion Of Different Datatypes to String:
+// 33 => "33";
+// 0 => "0"
+// null => "null"
+// undefined => "undefined"
+// true => "true"; false => "false"
