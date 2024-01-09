bc9c5575b12b86b7f479ac5a331bf8e3897c240c
Bikash <sahb2676@gmail.com>
2024-01-09 17:04:27 +0545

    while loop

---
+++ 05-Iterations/02-while-loop.js
@@ -0,0 +1,14 @@
+// while loop:
+let i = 1;
+while (i <= 10) {
+  console.log(`The value is ${i}.`);
+  i += 2;
+}
+
+// Using while loop to access array:
+let fruits = ["Apple", "Mango", "Litchi", "Guava", "Pineapple", "Coconut"];
+let index=0;
+while(index<fruits.length){
+    console.log(fruits[index]);
+    index++;
+}
